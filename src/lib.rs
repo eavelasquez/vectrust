78fc0b5bed3d6f6f6af0e12f9f629cca0b5eefc5
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-02-12 20:48:15 -0500

    test: update test case in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -26,16 +26,13 @@ impl<T> VectRust<T> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let mut vec = Vec::new();
-        vec.push(1usize);
-        vec.push(2);
-        vec.push(3);
-        vec.push(4);
-        vec.push(5);
+        let vec: VectRust<usize> = VectRust::new();
 
-        assert_eq!(vec.capacity(), 8);
-        assert_eq!(vec.len(), 5);
+        assert_eq!(vec.capacity(), 0);
+        assert_eq!(vec.len(), 0);
     }
 }

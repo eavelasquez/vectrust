02ec994966611379cbeea4d29773bec8df94fa60
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-02-12 20:46:59 -0500

    feat: add VectRust struct and basic methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+use std::ptr::NonNull;
+
+pub struct VectRust<T> {
+    capacity: usize,
+    len: usize,
+    ptr: NonNull<T>,
+}
+
+impl<T> VectRust<T> {
+    pub fn new() -> Self {
+        Self {
+            capacity: 0,
+            len: 0,
+            ptr: NonNull::dangling(),
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

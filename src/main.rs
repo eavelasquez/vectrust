ab2a5446df14907bb6f54deea28bc0a70eac72a2
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-02-17 18:28:50 -0500

    feat: add main.rs file with initial code

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use vectrust::VectRust;
+
+fn main() {
+    let mut vec: VectRust<usize> = VectRust::new();
+    vec.push(1usize);
+    vec.push(2);
+    vec.push(3);
+    vec.push(4);
+    vec.push(5);
+
+    assert_eq!(vec.capacity(), 8);
+    assert_eq!(vec.len(), 5);
+}

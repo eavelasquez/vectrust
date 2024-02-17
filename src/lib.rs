5b4bacff196a7a0aa3fe831454267065ecfd4e48
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-02-17 17:13:58 -0500

    feat: add push method to VectRust struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::ptr::NonNull;
+use std::{
+    alloc::{self, Layout},
+    mem,
+    ptr::NonNull,
+};
 
 pub struct VectRust<T> {
     capacity: usize,
@@ -12,6 +16,27 @@ impl<T> VectRust<T> {
             capacity: 0,
             len: 0,
             ptr: NonNull::dangling(),
+        }
+    }
+
+    pub fn push(&mut self, value: T) {
+        assert_ne!(mem::size_of::<T>(), 0, "No zero sized types");
+
+        if self.capacity == 0 {
+            let layout = Layout::array::<T>(4).expect("Could not allocate");
+
+            // SAFETY: the layout is hardcoded to be 4 * size_of<T> and
+            // size_of<T> is > 0.
+            let ptr = unsafe { alloc::alloc(layout) } as *mut T;
+            let ptr = NonNull::new(ptr).expect("Could not allocate memory");
+
+            // SAFETY: ptr is non-null and we have  just allocated enough space for
+            // this item (and 3 more). The memory previously at ptr is not read.
+            unsafe { ptr.as_ptr().write(value) };
+
+            self.ptr = ptr;
+            self.len = 1;
+            self.capacity = 4;
         }
     }
 
@@ -30,9 +55,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let vec: VectRust<usize> = VectRust::new();
+        let mut vec: VectRust<usize> = VectRust::new();
+        vec.push(1usize);
 
-        assert_eq!(vec.capacity(), 0);
-        assert_eq!(vec.len(), 0);
+        assert_eq!(vec.capacity(), 4);
+        assert_eq!(vec.len(), 1);
     }
 }
